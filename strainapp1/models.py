6bc911c896db7110eb11a8b9f23c421892f8260b
Julian <Schweiger.j88@gmail.com>
2023-05-04 21:37:06 +0200

    tabelle hinzugefuegt

--- strainapp1/models.py
+++ strainapp1/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Kommentar(models.Model):
+    ueberschrift = models.Charfield(max_length=30)
+    slug = models.CharField(max_length=30)
+    kommentar = models.TextField()
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
