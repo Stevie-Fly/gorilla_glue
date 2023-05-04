dc3f70d344be6f6d4d1c31ce736c2c66e477dfc6
Julian <Schweiger.j88@gmail.com>
2023-05-04 21:54:34 +0200

    neuer kommentar

--- strainapp1/models.py
+++ strainapp1/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Kommentar(models.Model):
-    ueberschrift = models.Charfield(max_length=30)
+    ueberschrift = models.CharField(max_length=30)
     slug = models.CharField(max_length=30)
     kommentar = models.TextField()
     name = models.CharField(max_length=50)
